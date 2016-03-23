58c75dc183799b74defa58201b1ad9ee5e5a9846
Lukas Lamm <lukas.lamm89@gmail.com>
2016-03-23 20:11:56 +0100

    Upgrade to 0.1.1
    Implement usage as module
    
    Include docs (via jsdoc)
    Include test

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 A command-line tool to convert musicJSON to abc notation
 An example of the used format of json is in [example.json](https://github.com/freakimkaefig/musicjson2abc/blob/master/example.json).
 
+## Example
+```javascript
+var musicjson2abc = require('musicjson2abc');
+
+var abc = musicjson2abc.convert2Abc('{"id":"123456","attributes":{"divisions":4,"clef":{"sign":"G","line":2},"key":{"fifths":-1},"time":{"beats":3,"beat-type":4}},"measures":[{"attributes":{"repeat":{"left":false,"right":false}},"notes":[{"type":"quarter","duration": 4,"rest": false,"pitch": {"step":"C","octave":4,"alter":0,"accidental": "flat"},"$$hashKey":"object:1255"}]}]}');
+
+console.log(abc);
+
+// output:
+//   X:123456
+//   T:123456
+//   M:3/4
+//   L:1/16
+//   K:F
+//    _C4|
+```
+
 ## Getting started
 Install the module with:
-```
+```sh
 npm install -g musicjson2abc
 ```
 
--- index.js
+++ index.js
@@ -1,237 +1,106 @@
-#!/usr/bin/env node
-var program = require('commander'),
-	chalk = require('chalk'),
-	fs = require('fs'),
-	mode,
-	inputFile,
-	outputFile,
+var chalk = require('chalk');
+var fs = require('fs');
+var nconf = require('nconf');
 
-	circle_of_fifths = {
-		"major": {
-			"-7": "B",
-			"-6": "F#",
-			"-5": "Db",
-			"-4": "Ab",
-			"-3": "Eb",
-			"-2": "Bb",
-			"-1": "F",
-			"0" : "C",
-			"1" : "G",
-			"2" : "D",
-			"3" : "A",
-			"4" : "E",
-			"5" : "B",
-			"6" : "F#",
-			"7" : "Db"
-		},
-		"minor": {
-			"-7": "G#",
-			"-6": "D#",
-			"-5": "Bb",
-			"-4": "F",
-			"-3": "C",
-			"-2": "G",
-			"-1": "D",
-			"0" : "A",
-			"1" : "E",
-			"2" : "B",
-			"3" : "F#",
-			"4" : "C#",
-			"5" : "G#",
-			"6" : "D#",
-			"7" : "Bb"
-		}
-	},
+nconf.file({ file: 'config.json' });
 
-	accidental = {
-		"flat-flat": "__",
-		"flat": "_",
-		"natural": "=",
-		"sharp" : "^",
-		"sharp-sharp" : "^^",
-		"undefined" : ""
-	},
+var mode;
+var circleOfFifths = nconf.get('circleOfFifths');
+var accidental = nconf.get('accidental');
+var pitches = nconf.get('pitches');
 
-	pitches = {
-		"1": {
-			"A": "A,,,",
-			"B": "B,,,",
-			"C": "C,,,",
-			"D": "D,,,",
-			"E": "E,,,",
-			"F": "F,,,",
-			"G": "G,,,"
-		},
-		"2": {
-			"A": "A,,",
-			"B": "B,,",
-			"C": "C,,",
-			"D": "D,,",
-			"E": "E,,",
-			"F": "F,,",
-			"G": "G,,"
-		},
-		"3": {
-			"A": "A,",
-			"B": "B,",
-			"C": "C,",
-			"D": "D,",
-			"E": "E,",
-			"F": "F,",
-			"G": "G,"
-		},
-		"4": {
-			"A": "A",
-			"B": "B",
-			"C": "C",
-			"D": "D",
-			"E": "E",
-			"F": "F",
-			"G": "G"
-		},
-		"5": {
-			"A": "a",
-			"B": "b",
-			"C": "c",
-			"D": "d",
-			"E": "e",
-			"F": "f",
-			"G": "g"
-		},
-		"6": {
-			"A": "a'",
-			"B": "b'",
-			"C": "c'",
-			"D": "d'",
-			"E": "e'",
-			"F": "f'",
-			"G": "g'"
-		},
-		"7": {
-			"A": "a''",
-			"B": "b''",
-			"C": "c''",
-			"D": "d''",
-			"E": "e''",
-			"F": "f''",
-			"G": "g''"
-		},
-		"8": {
-			"A": "a'''",
-			"B": "b'''",
-			"C": "c'''",
-			"D": "d'''",
-			"E": "e'''",
-			"F": "f'''",
-			"G": "g'''"
-		}
-	};
+/**
+ * Returns the abc notation string from given input
+ * @param {object} input - The parsed input from input file
+ * @retruns {string}
+ */
+function getAbcString(input) {
+  var outputData = "";
+  outputData += "X:"
+    + input.id
+    + "\n";
+  outputData += "T:"
+    + input.id
+    + "\n";
+  outputData += "M:"
+    + input.attributes.time.beats
+    + "/"
+    + input.attributes.time["beat-type"]
+    + "\n";
+  outputData += "L:"
+    + "1/"
+    + (input.attributes.divisions * input.attributes.time["beat-type"])
+    + "\n";
+  outputData += "K:"
+    + getAbcKey(input.attributes.key.fifths, input.attributes.key.mode)
+    + "\n";
 
-program
-	.version('0.0.1')
-	.arguments('<input> [output]')
-	.action(function(input, output) {
-		inputFile = input;
-		outputFile = output;
-	});
+  for (var i = 0; i < input.measures.length; i++) {
+    var measure = input.measures[i];
+    if (measure.attributes.repeat.left) {
+      outputData += "\n"
+        + "|:";
+    } else {
+      //outputData += "|";
+    }
 
-program.on('--help', function() {
-	console.log('  Examples:');
-	console.log('');
-	console.log('    $ musicjson2abc input.json output.abc');
-	console.log('    $ musicjson2abc example.json example.abc');
-	console.log('');
-	console.log('  Hint:');
-	console.log('');
-	console.log('    The input file should be a valid musicJSON file');
-	console.log('    The output file will become a valid abc file. May overwrite existing files.');
-	console.log('');
-});
+    for (var j = 0; j < measure.notes.length; j++) {
 
-program.parse(process.argv);
+      outputData += " "
+        + getAbcNote(measure.notes[j]);
+    }
 
-if (typeof inputFile === 'undefined') {
-	console.error(chalk.bold.red('ERROR: No input file specified.'));
-	console.info(chalk.cyan('Run musicjson2abc -h for further information.'));
-	process.exit(1);
-}
-if (typeof outputFile === 'undefined') {
-	outputFile = inputFile.replace(/\.[^/.]+$/, "") + ".abc";
-	console.info(chalk.cyan('INFO: No output file specified.'));
-	console.info(chalk.cyan('Defaults to', outputFile));
+    if (measure.attributes.repeat.right) {
+      outputData += ":|"
+        + "\n";
+    } else {
+      outputData += "|";
+    }
+
+  }
+
+  return outputData;
 }
 
-read_input(inputFile);
-
-
-function read_input(file) {
-	fs.readFile(file, 'utf8', function(err, data) {
-		if (err) {
-			console.error(chalk.bold.red('ERROR:', err));
-			process.exit(1);
-		}
-		convert(JSON.parse(data));
-	});
+/**
+ * Returns the key for abc notation from given fifths
+ * @param {number} fifths - The position inside the circle of fifths
+ * @param {string|undefined} mode - The mode (major / minor)
+ * @retruns {string}
+ */
+function getAbcKey(fifths, mode) {
+  if (typeof mode === 'undefined') mode = 'major';
+  return circleOfFifths[mode][fifths];
 }
 
-function convert(input) {
-	var outputData = "";
-	console.log(input);
-	outputData += "X:" + input.id + "\n";
-	outputData += "T:" + input.id + "\n";
-	outputData += "M:" + input.attributes.time.beats + "/" + input.attributes.time["beat-type"] + "\n";
-	outputData += "L:" + "1/" + (input.attributes.divisions * input.attributes.time["beat-type"]) + "\n";
-	outputData += "K:" + get_abc_key(input.attributes.key.fifths, input.attributes.key.mode) + "\n";
-	
-	for (var i = 0; i < input.measures.length; i++) {
-		var measure = input.measures[i];
-		if (measure.attributes.repeat.left) {
-			outputData += "\n" + "|:";
-		} else {
-			//outputData += "|";
-		}
-
-		for (var j = 0; j < measure.notes.length; j++) {
-
-			outputData += " " + get_abc_note(measure.notes[j]);
-		}
-
-		if (measure.attributes.repeat.right) {
-			outputData += ":|" + "\n";
-		} else {
-			outputData += "|";
-		}
-
-	}
-	console.log(outputData);
-
-	write_output(outputFile, outputData);
+/**
+ * Returns a note in abc notation from given note object (JSON)
+ * @param {object} note - The note that should be transformed to abc
+ * @returns {string}
+ */
+function getAbcNote(note) {
+  // check if rest
+  if (note.rest) {
+    // return rest as abc
+    return "z" + note.duration;
+  } else {
+    // return note as abc
+    return accidental[note.pitch.accidental]
+      + pitches[note.pitch.octave][note.pitch.step]
+      + note.duration;
+  }
 }
 
-function write_output(file, data) {
-	fs.writeFile(file, data, function(err) {
-		if(err) {
-			console.error(chalk.bold.red('ERROR:', err));
-			process.exit(1);
-		}
 
-		console.info(chalk.cyan('Output written to', outputFile));
-	});
-}
-
-function get_abc_key(fifths, mode) {
-	return circle_of_fifths[mode][fifths];
-}
-
-function get_abc_note(note) {
-	// check if rest
-	if (note.rest) {
-		// return rest as abc
-		return "z" + note.duration;
-	} else {
-		// return note as abc
-		return accidental[note.pitch.accidental] + pitches[note.pitch.octave][note.pitch.step] + note.duration;
-	}
+/**
+ * Returns a string in abc notation from given data
+ * @param {object} data - The JSON data that should be transformed to abc
+ * @returns {string}
+ */
+exports.convert2Abc = function(data) {
+  return getAbcString(JSON.parse(data));
 }
 
 // Run with: musicjson2abc example.json
+
+// Run jsdoc with: jsdoc index.js -d doc -R README.md
